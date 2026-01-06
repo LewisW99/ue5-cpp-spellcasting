e47285d476aa3b2d016f2649d3078dd4f29b05d1
Lewis Williams <60359937+LewisW99@users.noreply.github.com>
2026-01-06 10:13:44 +0000

    Upload Files

---
+++ SpellProjectile.cpp
@@ -0,0 +1,82 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SpellProjectile.h"
+
+#include "StatusEffectComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Health/HealthComponent.h"
+
+
+
+
+ASpellProjectile::ASpellProjectile()
+{
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	RootComponent = Mesh;
+
+	Mesh->SetCollisionProfileName(TEXT("OverlapAllDynamic"));
+	Mesh->SetGenerateOverlapEvents(true);
+
+	Movement = CreateDefaultSubobject<UProjectileMovementComponent>("Movement");
+	Movement->UpdatedComponent = Mesh;   
+	Movement->bRotationFollowsVelocity = true;
+	Movement->ProjectileGravityScale = 0.f;
+	Movement->InitialSpeed = 2000.f;
+	Movement->MaxSpeed = 1000000.f;
+
+	SetLifeSpan(3.5f);
+}
+
+void ASpellProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+	Mesh->OnComponentBeginOverlap.AddDynamic(this, &ASpellProjectile::OnOverlap);
+}
+
+void ASpellProjectile::Init(const FSpellStats& InStats)
+{
+	Damage = InStats.Damage;
+	EffectStrength = InStats.EffectStrength;
+	EffectDuration = InStats.EffectDuration;
+	
+	Movement->InitialSpeed = InStats.ProjectileSpeed;
+	Movement->MaxSpeed = InStats.ProjectileSpeed;
+
+	Movement->Velocity = GetActorForwardVector() * InStats.ProjectileSpeed;
+	UE_LOG(LogTemp, Warning, TEXT("Projectile speed: %f"),
+	Movement->Velocity.Size());
+}
+
+void ASpellProjectile::OnOverlap(
+	UPrimitiveComponent*,
+	AActor* OtherActor,
+	UPrimitiveComponent*,
+	int32,
+	bool,
+	const FHitResult&)
+{
+	if (!OtherActor || OtherActor == GetOwner())
+		return;
+
+	if (UHealthComponent* Health =
+		OtherActor->FindComponentByClass<UHealthComponent>())
+	{
+		Health->ApplyDamage(Damage);
+		if (Health->IsDead())
+		{
+			OtherActor->Destroy();
+		}
+	}
+	
+	if (UStatusEffectComponent* Effects =
+		OtherActor->FindComponentByClass<UStatusEffectComponent>())
+	{
+		Effects->ApplyEffect(SpellType, EffectStrength, EffectDuration);
+	}
+	
+	Destroy();
+}
+
+
+
